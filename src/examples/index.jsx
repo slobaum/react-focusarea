6ab058b598039b610b5a80fefca89e7cfc2ce9f3
slobaum <slobaum@gmail.com>
2019-11-04 17:12:54 -0600

    Added modal example, refining focus behavior

--- src/examples/index.jsx
+++ src/examples/index.jsx
@@ -9,7 +9,7 @@ const ExampleTriggerInside = () => {
     return (
         <FocusLostBox onFocusLost={() => setVisible(false)}>
             <button type="button" onClick={() => setVisible(true)}>
-                Show (with trigger inside box)
+                Show menu (with trigger inside box)
             </button>
             {visible && (
                 <div style={{backgroundColor: '#ccc'}}>
@@ -20,18 +20,19 @@ const ExampleTriggerInside = () => {
         </FocusLostBox>
     );
 };
+
 const ExampleTriggerOutside = () => {
     const [visible, setVisible] = React.useState(false);
 
     return (
         <div>
              <button type="button" onClick={() => setVisible(true)}>
-                Show (with autofocus [trigger outside box])
+                Show menu (with trigger outside box and autofocus)
              </button>
              {visible && (
                  <FocusLostBox
                       onFocusLost={() => setVisible(false)}
-                      focus={true}
+                      autoFocus={true}
                       style={{backgroundColor: '#ccc'}}
                  >
                      <a href="http://www.google.com">Google</a>
@@ -41,10 +42,54 @@ const ExampleTriggerOutside = () => {
         </div>
     );
 };
+
+const ExampleModal = () => {
+    const [visible, setVisible] = React.useState(false);
+
+    return (
+        <div>
+            <button type="button" onClick={() => setVisible(true)}>
+                Show modal
+            </button>
+            {visible && (
+                <div style={{
+                    backgroundColor: 'rgba(0, 0, 0, 0.25',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    position: 'fixed',
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                    left: 0,
+                }}>
+                    <FocusLostBox
+                        onFocusLost={() => setVisible(false)}
+                        autoFocus={true}
+                        style={{
+                            backgroundColor: 'white',
+                            padding: '2em',
+                        }}
+                    >
+                        This is a modal!
+                        <button
+                            type="button"
+                            onClick={() => setVisible(false)}
+                        >
+                            Okay!
+                        </button>
+                    </FocusLostBox>
+                </div>
+            )}
+        </div>
+    );
+};
+
 const App = () => (
     <React.Fragment>
         <ExampleTriggerInside />
         <ExampleTriggerOutside />
+        <ExampleModal />
     </React.Fragment>
 );
 
--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -2,31 +2,31 @@ import * as React from 'react';
 import propTypes from 'prop-types';
 
 
-export const FocusLostBox = ({onFocusLost, focus, ...props}) => {
-  const box = React.useRef(null);
-  const [hasMovedFocus, setHasMovedFocus] = React.useState(false);
+export const FocusLostBox = ({onFocusLost, autoFocus, ...props}) => {
+    const box = React.useRef(null);
+    const [hasMovedFocus, setHasMovedFocus] = React.useState(false);
 
-  React.useEffect(() => {
-      if (focus)
-          box.current.focus()
-  }, [box, focus]);
+    React.useEffect(() => {
+        if (autoFocus)
+            box.current.focus();
+    }, [box, autoFocus]);
 
-  var handleBlur = domEvent => {
-      const isOutsideBox = domEvent.relatedTarget && !box.current.contains(domEvent.relatedTarget);
+    var handleBlur = domEvent => {
+        const isOutsideBox = domEvent.relatedTarget && !box.current.contains(domEvent.relatedTarget);
 
-      setHasMovedFocus(true);
-      if (!domEvent.relatedTarget || isOutsideBox)
-          onFocusLost && onFocusLost();
-  };
+        setHasMovedFocus(true);
+        if (!domEvent.relatedTarget || isOutsideBox)
+            onFocusLost && onFocusLost();
+    };
 
-  return (
-       <div
-           {...props}
-           ref={box}
-           tabIndex={focus && !hasMovedFocus ? 0 : -1}
-           onBlur={handleBlur}
-       />
-  );
+    return (
+        <div
+            {...props}
+            ref={box}
+            onBlur={handleBlur}
+            tabIndex={hasMovedFocus ? -1 : 0}
+        />
+    );
 };
 FocusLostBox.propTypes = {
   onFocusLost: propTypes.func
