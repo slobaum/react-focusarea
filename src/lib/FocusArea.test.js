6ea726dcc0f28d4cc2bced9f559a45d240fb0a2f
slobaum <slobaum@gmail.com>
2019-11-26 12:03:57 -0600

    More tests

--- src/lib/FocusArea.test.js
+++ src/lib/FocusArea.test.js
@@ -35,3 +35,78 @@ describe('_computeStyle', () => {
         });
     });
 });
+
+describe('_computeTabIndex', () => {
+    test('should return 0 if allowDirectRefocus', () => {
+        expect(_computeTabIndex({allowDirectRefocus: true, hasMovedFocus: false}))
+            .toBe(0);
+    });
+    test('should return 0 if not hasMovedFocus', () => {
+        expect(_computeTabIndex({allowDirectRefocus: false, hasMovedFocus: false}))
+            .toBe(0);
+    });
+    test('should return -1 if not allowing direct refocus and has moved', () => {
+        expect(_computeTabIndex({allowDirectRefocus: false, hasMovedFocus: true}))
+            .toBe(-1);
+    });
+});
+
+describe('_makeBlurHandler', () => {
+    const _makeMockBox = contains => ({current: {contains: () => contains}});
+    const _makeMockDomEvent = relatedTarget => ({relatedTarget});
+
+    test('should return a function when invoked', () => {
+        expect(typeof(_makeBlurHandler({}))).toBe('function');
+    });
+
+    test('should invoke setHasMovedFocus with true', () => {
+        const setHasMovedFocus = jest.fn();
+
+        _makeBlurHandler({
+            setHasMovedFocus,
+            box: _makeMockBox(true),
+            onFocusLost: jest.fn(),
+        })(_makeMockDomEvent());
+
+        expect(setHasMovedFocus).toHaveBeenCalledWith(true);
+    });
+
+    describe('with relatedTarget', () => {
+        test('should invoke onFocusLost if box does not contain related target', () => {
+            const onFocusLost = jest.fn();
+
+            _makeBlurHandler({
+                onFocusLost,
+                setHasMovedFocus: jest.fn(),
+                box: _makeMockBox(false),
+            })(_makeMockDomEvent({}));
+
+            expect(onFocusLost).toHaveBeenCalledTimes(1);
+        });
+        test('should not invoke onFocusLost if box contains related target', () => {
+            const onFocusLost = jest.fn();
+
+            _makeBlurHandler({
+                onFocusLost,
+                setHasMovedFocus: jest.fn(),
+                box: _makeMockBox(true),
+            })(_makeMockDomEvent({}));
+
+            expect(onFocusLost).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('without relatedTarget', () => {
+        test('should invoke onFocusLost', () => {
+            const onFocusLost = jest.fn();
+
+            _makeBlurHandler({
+                onFocusLost,
+                setHasMovedFocus: jest.fn(),
+                box: _makeMockBox(),
+            })(_makeMockDomEvent({}));
+
+            expect(onFocusLost).toHaveBeenCalledTimes(1);
+        });
+    });
+});
